d0480b05250ce9b547ea6c84bd05e0f1fa719662
sahinmelikov <1rima621melikov@gmail.com>
2023-04-08 22:58:47 +0400

    kjbkj

---
+++ ClassConstructor/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Drawing;
+using System.Threading.Channels;
+using System.Xml.Linq;
+
+namespace ClassConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adinizi daxil edin");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadinizi daxil edin");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Grupunusu daxil edin");
+            string group = (Console.ReadLine());
+            Console.WriteLine("topladiginiz bali daxil edin");
+            int point = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mezun olmusunuz:True or False ");
+            string IsGraduated = Console.ReadLine();
+
+            Student student = new Student( name, surname, group, point,IsGraduated)
+            {
+                
+                
+                // name="sahin",
+                // surname="Melikov",
+                //group="jkhsknj",
+                //point=45,
+                //IsGraduated="True"
+            };
+
+            if (student.IsGraduated=="True")
+            {
+                Console.WriteLine("{");
+                Console.WriteLine($"Name:{student.name}");
+                Console.WriteLine($"Surname:{student.surname}");
+                Console.WriteLine($"group:{student.group}");
+                Console.WriteLine($"point:{student.point}");
+                Console.WriteLine($"IsGraduated:{student.IsGraduated}");
+                Console.WriteLine("}");
+                if (student.point > 80)
+                {
+                    Console.WriteLine($"{student.name} {student.surname} Siz Novbeti imtahana gire Bilersiniz");
+                }
+                else
+                {
+                    Console.WriteLine($"{student.name} {student.surname} Siz  Novbeti imtahana Gire Bilmezsiniz");
+                }
+            }
+            else
+            {
+                Console.WriteLine("{");
+                Console.WriteLine($"Name:{student.name}");
+                Console.WriteLine($"Surname:{student.surname}");
+                Console.WriteLine($"group:{student.group}");
+                Console.WriteLine($"point:{student.point}");
+                Console.WriteLine($"IsGraduated:{student.IsGraduated}");
+                Console.WriteLine("}");
+                Console.WriteLine($"{student.name} {student.surname} - Bu telebe artiq mezun olub");
+
+            }
+
+
+          
+        }
+    }
+}
---
+++ ClassConstructor/Student.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ClassConstructor
+{
+    public class Student
+    {
+       
+        public Student(string name,string surname,string group,int poin,string IsGraduated)
+        {
+           
+            this.name = name;
+            this.surname= surname;
+            this.group= group;
+            this.point= poin;
+            this.IsGraduated= IsGraduated;
+        }
+
+        public string name;
+        public string surname;
+        public string group;
+        public int point;
+        public string IsGraduated;
+    }
+   
+} 
